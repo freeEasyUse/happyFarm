449a80f018d1ce663ce1291fd5e082ff11a0455a
yekai123 <yekailoveqq@163.com>
2016-09-29 17:57:47 +0800

    商家用户管理更新

--- public/model/js/businessUserManager.js
+++ public/model/js/businessUserManager.js
@@ -23,59 +23,6 @@ farm.businessUser.initTable = function(tableId){
         field: '_id',
         title: 'id',
         visible:false
-    }, {
-        field: 'fieldName',
-        title: '地块名称',
-        visible:false
-    },
-     {
-        field: 'fieldCode',
-        title: '地块编号'
-    }
-    , {
-        field: 'fieldPartnerCode',
-        title: '商家编码'
-    },
-    {
-        field:'fieldPartnerName',
-        title:'商家名字'
-    },{
-        field:'fieldSize',
-         title:'地块大小'
-    },
-    {
-        field:'fieldCreateTime',
-        title:'创建时间',
-        formatter:function(value,row,index){
-            return farm.dateToStr(value);
-        }
-    },
-    {
-        field:'fieldUpdateTime',
-        title:'修改时间',
-        formatter:function(value,row,index){
-            return farm.dateToStr(value);
-        }
-    },
-    {
-        field:'fieldVedio',
-        title:'监控设备'
-    },
-    {
-        field:'fieldStatus',
-        title:'当前状态'
-    },
-    {
-        field:'fieldUserCode',
-        title:'当前使用人编码'
-    },
-    {
-        field:'fieldUserName',
-        title:'当前使用人'
-    },
-    {
-        field:'fieldDes',
-        title:'描述'
     }];
 
     settingParam.toolbar = '#field_toolbar';
