3582e84c2fed231275f0f93431528761b792d816
yekai123 <yekailoveqq@163.com>
2016-10-10 16:57:38 +0800

    定义方法

--- common/timer.js
+++ common/timer.js
@@ -5,10 +5,21 @@ var schedule = require("node-schedule");
 
 var timer = new Object();
 
+/**
+ * 定时任务测试
+ */
 timer.test1 = function(){
     schedule.scheduleJob('* * * * * *', function(){
     console.log('The answer to life, the universe, and everything!');
 });
+}
+
+
+/**
+ * 查询到期租赁关系 每天 23 时 执行
+ */
+timer.timeUpField = function(){
+
 }
 
 
